e32d173494f86c48a6d722b15c2d4985d9be79d4
Ricardo Godinho <ricardo.godinho92@hotmail.com>
2016-11-04 17:39:16 +0000

    Adicionei função que permite ir buscar o conteúdo estatico da página como imagens ou mesmo os ficheiros do bootstrap.

--- wc/src/main/java/corp/adentis/wc/configurations/WebConfiguration.java
+++ wc/src/main/java/corp/adentis/wc/configurations/WebConfiguration.java
@@ -3,6 +3,7 @@ package corp.adentis.wc.configurations;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.JstlView;
@@ -26,6 +27,11 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 		registry.viewResolver(viewResolver);
 		
 		
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
 	}
 	
 }
